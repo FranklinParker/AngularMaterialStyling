11ae7a5869ddbb18e0e2c392c6a2837ef61bbf83
Franklin Parker <fparker@deliveryman.com>
2019-03-07 16:51:00 -0500

    product card

---
+++ src/app/orders/components/orders-home/orders-products/orders-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-products',
+  templateUrl: './orders-products.component.html',
+  styleUrls: ['./orders-products.component.scss']
+})
+export class OrdersProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrdersHomeComponent } from './components/orders-home/orders-home.component';
 import {SharedModule} from '../shared/shared.module';
+import { OrdersProductsComponent } from './components/orders-home/orders-products/orders-products.component';
 
 @NgModule({
-  declarations: [OrdersHomeComponent],
+  declarations: [OrdersHomeComponent, OrdersProductsComponent],
   imports: [
     CommonModule,
     OrdersRoutingModule,
