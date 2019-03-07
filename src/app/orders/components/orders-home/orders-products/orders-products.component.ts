93988d6b9bb27303f3d5d3dbd1ab0db1118cab00
Franklin Parker <fparker@deliveryman.com>
2019-03-07 18:18:35 -0500

    flip the card

--- src/app/orders/components/orders-home/orders-products/orders-products.component.ts
+++ src/app/orders/components/orders-home/orders-products/orders-products.component.ts
@@ -1,16 +1,39 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Product} from '../../../models/product';
+import {animate, state, style, transition, trigger,} from '@angular/animations';
 
 @Component({
   selector: 'app-orders-products',
   templateUrl: './orders-products.component.html',
-  styleUrls: ['./orders-products.component.scss']
+  styleUrls: ['./orders-products.component.scss'],
+  animations: [
+    trigger('cardSideState', [
+      state('front',
+        style({
+          transform: 'rotateY(0)'
+        })
+      ),
+      state('back',
+        style({
+          transform: 'rotateY(180deg)'
+        })
+      ),
+      transition('* => *', animate('500ms'))
+    ])
+  ]
 })
 export class OrdersProductsComponent implements OnInit {
   @Input() product: Product;
-  constructor() { }
+  cardSide = 'front';
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onFlip() {
+    this.cardSide = this.cardSide === 'front' ? 'back' : 'front';
   }
 
 }
