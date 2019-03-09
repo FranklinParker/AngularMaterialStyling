468e960dbf9cce599e1740552ad059968ba37671
Franklin Parker <fparker@deliveryman.com>
2019-03-09 18:41:52 -0500

    save

--- src/app/orders/components/orders-home/product-selected/product-selected.component.ts
+++ src/app/orders/components/orders-home/product-selected/product-selected.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Product} from '../../../models/product';
 
 @Component({
@@ -9,6 +9,7 @@ import {Product} from '../../../models/product';
 
 export class ProductSelectedComponent implements OnInit {
   @Input() product: Product;
+  @Output() closePopup = new EventEmitter<void>()
 
   constructor() {
   }
@@ -17,7 +18,7 @@ export class ProductSelectedComponent implements OnInit {
   }
 
   close() {
-    alert('hi');
+    this.closePopup.emit();
   }
 
 }
