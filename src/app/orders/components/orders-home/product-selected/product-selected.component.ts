b061cbcf9eaef5d66e5e64f5db9af0f4b12a60d6
Franklin Parker <fparker@deliveryman.com>
2019-03-09 18:08:51 -0500

    save

--- src/app/orders/components/orders-home/product-selected/product-selected.component.ts
+++ src/app/orders/components/orders-home/product-selected/product-selected.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Product} from '../../../models/product';
 
 @Component({
   selector: 'app-product-selected',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ProductSelectedComponent implements OnInit {
-  showSelectedProduct = true;
+  @Input() product: Product;
   constructor() { }
 
   ngOnInit() {
