b0989a651bb55a72d18d3f1b5d986d3a11854bc0
Franklin Parker <fparker@deliveryman.com>
2019-03-11 21:39:44 -0400

    add flex items

--- src/app/flex/flex.module.ts
+++ src/app/flex/flex.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { FlexRoutingModule } from './flex-routing.module';
 import { FlexHomeComponent } from './components/flex-home/flex-home.component';
 import {SharedModule} from '../shared/shared.module';
+import { FlexItemComponent } from './components/flex-item/flex-item.component';
 
 @NgModule({
-  declarations: [FlexHomeComponent],
+  declarations: [FlexHomeComponent, FlexItemComponent],
   imports: [
     CommonModule,
     FlexRoutingModule,
