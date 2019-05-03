52e59ea0fd8eff9b91e9ecad5cf843319bc41f77
Franklin Parker <fparker@deliveryman.com>
2019-05-03 10:22:44 -0400

    responsive image

--- src/app/orders/components/orders-home/orders-products/orders-products.component.ts
+++ src/app/orders/components/orders-home/orders-products/orders-products.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Product} from '../../../models/product';
-import {animate, state, style, transition, trigger,} from '@angular/animations';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-orders-products',
