cf11d246dd2defb1d27a6da973de9f10b8a6b1ab
Franklin Parker <fparker@deliveryman.com>
2019-05-09 19:03:57 -0400

    adjustex

--- src/app/floating/animation/animationTriggers.ts
+++ src/app/floating/animation/animationTriggers.ts
@@ -9,10 +9,10 @@ import {
 export const expandPanelStateTrigger = trigger('expandPanelState', [
   transition(':enter', [
     style({
-      transform: 'translateY(-50%) scale(.2)',
-      opacity: 0
+      transform: 'translateY(-50%)',
+      opacity: 1
     }),
-    animate('700ms ease-in')
+    animate('500ms ease-in')
   ]),
   transition(':leave', animate('350ms ease-out', style({
     opacity: 0,
@@ -29,8 +29,8 @@ export const shrunkPanelStateTrigger = trigger('shrunkPanelState', [
     }),
     animate('700ms ease-in')
   ]),
-  transition(':leave', animate('350ms ease-out', style({
-    opacity: 0,
+  transition(':leave', animate('700ms ease-out', style({
+    opacity: 1,
 
   })))
 ]);
