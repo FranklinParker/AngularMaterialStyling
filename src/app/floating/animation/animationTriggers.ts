4ffdce26332841185210afd13c59adbd3405373f
Franklin Parker <fparker@deliveryman.com>
2019-05-09 18:15:31 -0400

    animation

--- src/app/floating/animation/animationTriggers.ts
+++ src/app/floating/animation/animationTriggers.ts
@@ -1,72 +1,22 @@
-import {animate, state, style, transition, trigger, group, keyframes} from '@angular/animations';
+import {
+  animate,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 
 
-export const showStateTrigger = trigger('showState', [
+export const expandPanelStateTrigger = trigger('expandPanelState', [
   transition(':enter', [
     style({
-      transform: 'scale(.2)',
+      transform: 'translateY(-50%) scale(.2)',
       opacity: 0
     }),
-    animate('350ms ease-in')
+    animate('700ms ease-in')
   ]),
   transition(':leave', animate('350ms ease-out', style({
     opacity: 0,
-    transform: 'scale(.2)',
+    transform: 'translateY(-50%) scale(.2)',
 
   })))
 ]);
-
-export const animateStateTrigger = trigger('animateState', [
-  transition('* => *', [
-    animate('4000ms cubic-bezier(0,.9,1,.15)', style({
-      width: 0
-    })),
-    animate(400, style({
-      width: '*'
-    }))
-  ])
-]);
-
-export const listStateTrigger = trigger('listState', [
-  // transition('void => *', [
-  //   style({
-  //     opacity: 0
-  //   }),
-  //   animate(300)
-  // ]),
-  // transition('* => void', animate(300, style({
-  //   opacity: 0
-  // })))
-  transition(':enter', [
-    style({
-      opacity: 0,
-      backgroundColor: 'white'
-    }),
-    group([
-      animate(1000, style({
-        opacity: 0.7
-      })),
-      animate('5000ms ease-out', keyframes([
-        style({
-          backgroundColor: 'white',
-          offset: 0
-        }),
-        style({
-          backgroundColor: 'red',
-          offset: 0.8
-        }),
-        style({
-          backgroundColor: 'green',
-          offset: 1
-        })
-      ]))
-    ]),
-    animate(300, style({
-      backgroundColor: 'lightblue'
-    }))
-  ]),
-  transition(':leave', animate(300, style({
-    opacity: 0
-  })))
-]);
-
--- src/app/floating/components/floating-home/floating-home.component.ts
+++ src/app/floating/components/floating-home/floating-home.component.ts
@@ -1,9 +1,13 @@
 import {Component, OnInit} from '@angular/core';
+import {expandPanelStateTrigger} from '../../animation/animationTriggers';
 
 @Component({
   selector: 'app-floating-home',
   templateUrl: './floating-home.component.html',
-  styleUrls: ['./floating-home.component.scss']
+  styleUrls: ['./floating-home.component.scss'],
+  animations: [
+    expandPanelStateTrigger
+  ]
 })
 export class FloatingHomeComponent implements OnInit {
   expanded: boolean = false;
