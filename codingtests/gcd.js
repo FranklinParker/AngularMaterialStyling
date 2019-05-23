e38e031e348f19cf09c67432df93484b82ae7a89
Franklin Parker <fparker@deliveryman.com>
2019-05-23 18:04:48 -0400

    save

--- codingtests/gcd.js
+++ codingtests/gcd.js
@@ -9,10 +9,24 @@
  * @return {number} The largest integer that evenly divides both numbers
  */
 const gcd = (a, b) => {
-	// Please implement this.
+  a = Math.abs(a);
+  b = Math.abs(b);
+  if (b > a) {var temp = a; a = b; b = temp;}
+  while (true) {
+    console.log('a:' + a + ' b:' +b);
+
+    if (b == 0) return a;
+    a %= b;
+
+    if (a == 0) return b;
+    b %= a;
+  }
 };
 
 console.log("gcd(60, 500) = 20:", gcd(60, 500));
 console.log("gcd(3125, 100) = 25:", gcd(3125, 100));
 console.log("gcd(32, 8) = 8:", gcd(32, 8));
 console.log("gcd(4, 9) = 1:", gcd(4, 9));
+console.log("gcd(3, 8) = 1:", gcd(3, 8));
+
+
