48d5f3ab037f4416a1f07287f83d4d8d9ce010d7
FranklinParker <fparker@deliveryman.com>
2020-12-24 12:17:01 -0500

    save

--- src/app/grid/components/grid-min-max/grid-min-max.component.spec.ts
+++ src/app/grid/components/grid-min-max/grid-min-max.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GridMinMaxComponent } from './grid-min-max.component';
 
@@ -6,7 +6,7 @@ describe('GridMinMaxComponent', () => {
   let component: GridMinMaxComponent;
   let fixture: ComponentFixture<GridMinMaxComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GridMinMaxComponent ]
     })
